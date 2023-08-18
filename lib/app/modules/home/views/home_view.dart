ece8d6181414ba303c493a96adb5d382100cd620
Nwarapork <142137483+Nwarapork@users.noreply.github.com>
2023-08-18 18:21:59 +0700

    getX architecture
    get_cli build getX architecture

---
+++ lib/app/modules/home/views/home_view.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+import '../controllers/home_controller.dart';
+
+class HomeView extends GetView<HomeController> {
+  const HomeView({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('HomeView'),
+          centerTitle: true,
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+                child: Obx(
+              () => Text(
+                '📎 Count : ${controller.count.value}',
+                style: const TextStyle(fontSize: 20),
+              ),
+            )),
+            ElevatedButton.icon(
+                onPressed: () {
+                  controller.increment();
+                },
+                icon: const Icon(Icons.plus_one),
+                label: const Text('Plus 1️⃣'))
+          ],
+        ));
+  }
+}
